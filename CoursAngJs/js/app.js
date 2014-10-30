8c499d5e046e38585cc7e977a5c455e27e48c2f7
boomjinx <thimir92@gmail.com>
2014-10-30 23:32:50 +0100

    dev 0.2
    Angular cours 1.7

--- CoursAngJs/js/app.js
+++ CoursAngJs/js/app.js
@@ -1,1 +1,25 @@
-var app = angular.module('store', [ ]);
+(function(){
+	var app = angular.module('store', [ ]);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: 'Mot Mot Mot Virgule Mot Mot Mot Mot Point' ,
+			canPurchase: true,
+			soldOut: false,
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: 'Mot Mot Mot Virgule Mot Mot Mot Mot Point' ,
+			canPurchase: false,
+			soldOut: false,
+		}
+	]
+
+})();
